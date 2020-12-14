bdf0e27411928000bfb0c500c4ad5286728a11e3
Fabio C <s276842@studenti.polito.it>
2020-12-14 18:10:57 +0100

    PACS class

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import logging
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Subset, DataLoader
+from torch.backends import cudnn
+from torch.autograd import Function
+from torchvision.models.utils import load_state_dict_from_url
+
+import torchvision
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+
+from PIL import Image
+from tqdm import tqdm
+
+import numpy as np
+
+from sklearn.model_selection import ParameterGrid
+
+import matplotlib.pyplot as plt
+
+# Clone github repository with data
+# if not os.path.isdir('./Homework3-PACS'):
+#   !git clone https://github.com/MachineLearning2020/Homework3-PACS.git
+
+transformation = transforms.Compose([ transforms.Resize(230),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)) # Normalizes tensor with mean and standard deviation of IMAGENET
+])
+
+PACS_PATH = 'Homework3-PACS/PACS/'
+
+art_painting_dataset=ImageFolder(PACS_PATH + 'art_painting',transform=transformation)
+cartoon_dataset=ImageFolder(PACS_PATH + 'cartoon',transform=transformation)
+photo_dataset=ImageFolder(PACS_PATH + 'photo',transform=transformation)
+sketch_dataset=ImageFolder(PACS_PATH + 'sketch',transform=transformation)
---
+++ pacs_dataset.py
@@ -0,0 +1,75 @@
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+import os
+class PACS():
+    def __init__(self, root, transform=None, batch_size=64, num_workers=0):
+
+        # Load all four datasets with ImageFolder
+        self.__art_dataset = ImageFolder(os.path.join(root,'art_painting'), transform=transform)
+        self.__cartoon_dataset = ImageFolder(os.path.join(root,'cartoon'), transform=transform)
+        self.__photo_dataset = ImageFolder(os.path.join(root,'photo'), transform=transform)
+        self.__sketch_dataset = ImageFolder(os.path.join(root,'sketch'), transform=transform)
+
+        # Create dataloader for each dataset
+        self.__art_dataloader = DataLoader(self.__art_dataset, batch_size=batch_size, shuffle=True,
+                                           num_workers=num_workers, drop_last=True)
+        self.__cartoon_dataloader = DataLoader(self.__cartoon_dataset, batch_size=batch_size, shuffle=True,
+                                            num_workers=num_workers, drop_last=True)
+        self.__photo_dataloader = DataLoader(self.__photo_dataset, batch_size=batch_size, shuffle=True,
+                                            num_workers=num_workers, drop_last=True)
+        self.__sketch_dataloader = DataLoader(self.__sketch_dataset, batch_size=batch_size, shuffle=True,
+                                            num_workers=num_workers, drop_last=True)
+
+        # Classes (same for all dataset)
+        self.classes = self.__art_dataset.classes
+
+        # Dictionary to access the dataset/dataloaders
+        self.dic = {'p':self.__photo_dataloader, 'photo':self.__photo_dataloader,
+                    'a':self.__art_dataloader, 'art':self.__art_dataloader,
+                    'c':self.__cartoon_dataloader, 'cartoon':self.__cartoon_dataloader,
+                    's':self.__sketch_dataloader, 'sketch':self.__sketch_dataloader}
+
+
+
+    def __getitem__(self, item):
+        if type(item) is str:
+            return self.dic.get(item[0].lower()).dataset
+        if type(item) is tuple:
+            try:
+                return self.dic.get(item[0].lower()).dataset.__getitem__(item[1])
+            except:
+                pass
+
+    def get_next_batch(self, cat):
+        try:
+            return next(iter(self.dic.get(cat.lower())))
+        except:
+            pass
+
+
+    def load(self, cat):
+        for batch in self.dic[cat.lower()]:
+            yield batch
+
+
+
+if __name__ == '__main__':
+    import torchvision
+    from torchvision import transforms
+    imgnet_mean, imgnet_std = (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
+
+    # Define transforms for training phase
+    train_transform = transforms.Compose([transforms.Resize(256),transforms.CenterCrop(224),transforms.ToTensor(),transforms.Normalize(imgnet_mean, imgnet_std)])
+
+
+    pacs = PACS('Homework3-PACS\PACS', transform=train_transform, batch_size=512, num_workers=4)
+    # print(pacs['A',-1])
+
+    # print(pacs.get_next_batch('art'))
+    import matplotlib.pyplot as plt
+    for batch in pacs.load('cartoon'):
+        plt.imshow(batch[0][0].permute(1,2,0))
+        plt.show()
+        print(pacs.classes[batch[1][0]])
